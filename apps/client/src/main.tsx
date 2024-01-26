4a34e24b41529742b4925bef975f8500dfa11a2c
CanseFr <jucansell@gmail.com>
2024-01-26 17:08:54 +0100

    :lipstick: Setup Mui Theme Provider

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import {App} from "./App.tsx";
+import {createTheme, ThemeProvider} from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3a3838'
+    },
+    secondary: {
+      main: '#b293d7'
+    }
+  }
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
+
