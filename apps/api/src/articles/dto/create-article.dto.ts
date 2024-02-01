e3f86b6783cc7afe1df39539df718fd709e41fad
CanseFr <jucansell@gmail.com>
2024-02-01 22:44:21 +0100

    :zap: Upgrade Global pipe with whitelist, to secure data like create and update at in db

--- apps/api/src/articles/dto/create-article.dto.ts
+++ apps/api/src/articles/dto/create-article.dto.ts
@@ -1,21 +1,36 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsNotEmpty, IsNumber, IsString, MinLength } from 'class-validator';
 
 export class CreateArticleDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
   @ApiProperty()
   title: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
   @ApiProperty({ required: false })
   description: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
   @ApiProperty()
   reference: string;
 
+  @IsNumber()
+  @IsNotEmpty()
   @ApiProperty()
   price: number;
 
+  @IsNumber()
+  @IsNotEmpty()
   @ApiProperty()
   stock: number;
 
+  @IsBoolean()
   @ApiProperty({ required: false, default: false })
   published?: boolean = false;
 }
