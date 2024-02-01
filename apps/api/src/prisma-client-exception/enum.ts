c7135b12d666d08f7fb4f6ff7b0d46ea857353c7
CanseFr <jucansell@gmail.com>
2024-02-01 23:14:57 +0100

    :zap: Clean all project with the Project Errors : Inspect Code, EsLint and remove unecessary file

--- apps/api/src/prisma-client-exception/enum.ts
+++ apps/api/src/prisma-client-exception/enum.ts
@@ -6,23 +6,23 @@ export enum PrismaClientValidationError {
    * P2002 - "Unique constraint failed on the {constraint}".
    * Used to handle unique constraint violation errors.
    */
-  P2002 = "P2002",
+  P2002 = 'P2002',
 
   /**
    * P2001 - ""The record searched for in the where condition ({model_name}.{argument_name} = {argument_value}) does not exist"".
    * Used to handle entity not found.
    */
-  P2001 = "P2001",
+  P2001 = 'P2001',
 
   /**
    * P2022 - Handle foreign key constraint violation.
    * This error occurs when there is a violation of a foreign key constraint.
    */
-  P2022 = "P2022",
+  P2022 = 'P2022',
 
   /**
    * P2025 - Handle unique constraint violation.
    * This error occurs when there is a violation of a unique constraint.
    */
-  P2025 = "P2025",
+  P2025 = 'P2025',
 }
