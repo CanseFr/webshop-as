2e2d0a17f0f89fe1d925d9ecd5c83841cf0f338e
CanseFr <jucansell@gmail.com>
2024-02-03 01:08:25 +0100

    :card_file_box: Update schema with gallery

--- apps/api/prisma/migrations/20240130102238_init_article_unic_on_ref/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[reference]` on the table `Article` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `Article_reference_key` ON `Article`(`reference`);
