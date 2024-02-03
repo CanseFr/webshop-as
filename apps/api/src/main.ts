fe6d166023ad2c2724acf0a88addc44184e28269
CanseFr <jucansell@gmail.com>
2024-02-03 01:40:37 +0100

    :lock: Enable Cors from client to api + fetch url article get all

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,6 +6,12 @@ import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-cl
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
+
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
--- apps/client/src/pages/shop/index.tsx
+++ apps/client/src/pages/shop/index.tsx
@@ -1,3 +1,8 @@
+import {useEffect} from "react";
+
 export const Shop = () => {
+  useEffect(()=>{
+    fetch('http://localhost:3000/api/articles').then((res)=>res.text()).then((data)=>console.log(data))
+  })
   return <p>Shop</p>;
 };
