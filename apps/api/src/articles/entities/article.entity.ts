fa732fe664ac97de6d48d0cca3b12e1d80cab436
CanseFr <jucansell@gmail.com>
2024-02-03 01:11:17 +0100

    :sparkles: Module, entity, dto, service and controller about gallery, with nested relation apply on article service to have gallery with

--- apps/api/src/articles/entities/article.entity.ts
+++ apps/api/src/articles/entities/article.entity.ts
@@ -1,23 +1,38 @@
 import { Article } from '@prisma/client';
 import { ApiProperty } from '@nestjs/swagger';
+import { GalleryEntity } from '../../gallery/entities/gallery.entity';
 
 export class ArticleEntity implements Article {
   @ApiProperty()
   createdAt: Date;
+
   @ApiProperty()
   description: string;
+
   @ApiProperty()
   id: number;
+
   @ApiProperty()
   price: number;
+
   @ApiProperty()
   published: boolean;
+
   @ApiProperty()
   reference: string;
+
   @ApiProperty()
   stock: number;
+
   @ApiProperty()
   title: string;
+
   @ApiProperty()
   updatedAt: Date;
+
+  @ApiProperty()
+  galleryId: number | null;
+
+  @ApiProperty({ required: false, type: GalleryEntity })
+  gallery: GalleryEntity;
 }
---
+++ apps/api/src/gallery/gallery.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateGalleryDto } from './dto/create-gallery.dto';
+import { UpdateGalleryDto } from './dto/update-gallery.dto';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class GalleryService {
+  constructor(private prisma: PrismaService) {}
+
+  create(createGalleryDto: CreateGalleryDto) {
+    return this.prisma.gallery.create({ data: createGalleryDto });
+  }
+
+  findAll() {
+    return this.prisma.gallery.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prisma.gallery.findUnique({ where: { id } });
+  }
+
+  update(id: number, updateGalleryDto: UpdateGalleryDto) {
+    return this.prisma.gallery.update({ where: { id }, data: updateGalleryDto });
+  }
+
+  remove(id: number) {
+    return this.prisma.gallery.delete({ where: { id } });
+  }
+}
