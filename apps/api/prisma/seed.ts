b561aa0aa0fdb3178ff4e6d0caae6e6652402c02
CanseFr <jucansell@gmail.com>
2024-02-03 01:08:57 +0100

    :seedling: Update seed with gallery add to article

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -4,7 +4,52 @@ import { PrismaClient } from '@prisma/client'; // initialize Prisma Client
 const prisma = new PrismaClient();
 
 async function main() {
-  // create two dummy articles
+  // IMG Article
+  const img1 = await prisma.gallery.upsert({
+    where: { thumbnail_path: 'assets/article/TB001KAN0.png' },
+    update: {},
+    create: {
+      thumbnail_path: 'assets/article/TB001KAN0.png',
+      gal_1_path: 'assets/article/TB001KAN1.png',
+      gal_2_path: 'assets/article/TB001KAN2.png',
+      gal_3_path: 'assets/article/TB001KAN3.png',
+    },
+  });
+
+  const img2 = await prisma.gallery.upsert({
+    where: { thumbnail_path: 'assets/article/BN001KNO0.png' },
+    update: {},
+    create: {
+      thumbnail_path: 'assets/article/BN002KNO0.png',
+      gal_1_path: 'assets/article/BN002KNO1.png',
+      gal_2_path: 'assets/article/BN002KNO2.png',
+      gal_3_path: 'assets/article/BN002KNO3.png',
+    },
+  });
+
+  const img3 = await prisma.gallery.upsert({
+    where: { thumbnail_path: 'assets/article/BT003KNO0.png' },
+    update: {},
+    create: {
+      thumbnail_path: 'assets/article/BT003KNO0.png',
+      gal_1_path: 'assets/article/BT003KNO1.png',
+      gal_2_path: 'assets/article/BT003KNO2.png',
+      gal_3_path: 'assets/article/BT003KNO3.png',
+    },
+  });
+
+  const img4 = await prisma.gallery.upsert({
+    where: { thumbnail_path: 'assets/article/BN004KNO0.png' },
+    update: {},
+    create: {
+      thumbnail_path: 'assets/article/BN004KNO0.png',
+      gal_1_path: 'assets/article/BN004KNO1.png',
+      gal_2_path: 'assets/article/BN004KNO2.png',
+      gal_3_path: 'assets/article/BN004KNO3.png',
+    },
+  });
+
+  // Articles
   const post1 = await prisma.article.upsert({
     where: { title: 'Black Kanji Japan' },
     update: {},
@@ -15,6 +60,7 @@ async function main() {
       price: 21.5,
       stock: 20,
       published: true,
+      galleryId: img1.id,
     },
   });
 
@@ -28,6 +74,7 @@ async function main() {
       price: 20,
       stock: 20,
       published: true,
+      galleryId: img2.id,
     },
   });
 
@@ -41,6 +88,7 @@ async function main() {
       price: 19,
       stock: 20,
       published: true,
+      galleryId: img3.id,
     },
   });
 
@@ -54,10 +102,12 @@ async function main() {
       price: 22,
       stock: 20,
       published: false,
+      galleryId: img4.id,
     },
   });
 
   console.log({ post1, post2, post3, post4 });
+  console.log({ img1, img2, img3, img4 });
 }
 
 // execute the main function
