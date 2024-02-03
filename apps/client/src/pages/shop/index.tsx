11ba2a41d4c1813070392be81092a538be0ab3a2
CanseFr <jucansell@gmail.com>
2024-02-03 03:14:15 +0100

    :sparkles: Create a loader during fetching data

--- apps/client/src/pages/shop/index.tsx
+++ apps/client/src/pages/shop/index.tsx
@@ -1,17 +1,26 @@
 import { useEffect, useState } from 'react';
 import { ArticleEntity } from 'api/dist/src/articles/entities/article.entity.ts';
 import { ArticleCard } from '../../components/cards/article-card.tsx';
+import { CircularProgress, Grid } from '@mui/material';
 
 export const Shop = () => {
   const [articleList, setArticleList] = useState<ArticleEntity[]>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
+    setIsLoading(true);
     fetch('http://localhost:3000/api/articles')
       .then((res) => res.json())
       .then((data) => setArticleList(data));
+    setIsLoading(false);
   }, []);
 
-  if (!articleList) return;
+  if (!articleList)
+    return (
+      <Grid display="flex" justifyContent="center" marginTop={40}>
+        <CircularProgress size={80} />
+      </Grid>
+    );
 
   return <ArticleCard articleList={articleList} />;
 };
