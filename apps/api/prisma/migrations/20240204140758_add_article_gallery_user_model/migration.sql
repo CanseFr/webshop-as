a16b8716f0e1aa7ae8060df2f5c9ad9913d6c8c4
CanseFr <jucansell@gmail.com>
2024-02-04 15:10:23 +0100

    :card_file_box: Upgrade schema and his seeds

--- apps/api/prisma/migrations/20240204140758_add_article_gallery_user_model/migration.sql
+++ apps/api/prisma/migrations/20240204140758_add_article_gallery_user_model/migration.sql
@@ -33,5 +33,23 @@ CREATE TABLE `Gallery` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `lastname` VARCHAR(191) NOT NULL,
+    `firstname` VARCHAR(191) NOT NULL,
+    `birthday` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `role` VARCHAR(191) NOT NULL,
+    `avatarPath` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User_avatarPath_key`(`avatarPath`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `Article` ADD CONSTRAINT `Article_galleryId_fkey` FOREIGN KEY (`galleryId`) REFERENCES `Gallery`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
