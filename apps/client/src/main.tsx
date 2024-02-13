74895a65d692c424e825a69239d6d42f63d49267
CanseFr <jucansell@gmail.com>
2024-02-13 15:31:23 +0100

    :rotating_light: EsLint

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { App } from './App.tsx';
 import { createTheme, ThemeProvider } from '@mui/material';
 import { Provider } from 'react-redux';
-import store from "./store";
+import store from './store';
 
 const theme = createTheme({
   palette: {
