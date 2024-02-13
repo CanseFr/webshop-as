f8e4804ed988913eb55df3477c309436d92c2b26
CanseFr <jucansell@gmail.com>
2024-02-13 14:39:56 +0100

    :sparkles: Provide context store

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App.tsx';
 import { createTheme, ThemeProvider } from '@mui/material';
+import { Provider } from 'react-redux';
+import store from "./store";
 
 const theme = createTheme({
   palette: {
@@ -19,7 +21,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>,
 );
