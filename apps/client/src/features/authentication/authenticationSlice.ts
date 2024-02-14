eada90cea2e31d258de90a6745ea5e614810f6b7
CanseFr <jucansell@gmail.com>
2024-02-14 12:39:44 +0100

    :sparkles: Ajust slice about auth

--- apps/client/src/features/authentication/authenticationSlice.ts
+++ apps/client/src/features/authentication/authenticationSlice.ts
@@ -1,37 +1,91 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { LoginDto } from 'api/dist/src/auth/dto/login.dto';
+import { jwtDecode } from 'jwt-decode';
 
 const initialState = {
-  user: '',
   isAuthenticated: false,
-  email: '',
-  password: '',
+  tokenHashed: '',
+
   role: '',
+
+  userId: null,
+  avatarPath: '',
+  email: '',
+  firstname: '',
+  lastname: '',
+  exp: null,
+  iat: null,
+
+  errorFetchingLogin: '',
 };
 
 const authenticateSlice = createSlice({
   name: 'authenticate',
   initialState,
   reducers: {
-    login: {
-      prepare(email: string, password: string): any {
-        return {
-          payload: { email, password },
-        };
-      },
-      reducer(state, action) {
-        state.user = 'Canse';
-        state.isAuthenticated = true;
-        state.email = action.payload.email;
-        state.password = action.payload.password;
-        state.role = 'ADMIN';
-      },
+    login() {
+      // console.log(action.payload);
+      // console.log(state);
+    },
+    saveToken(state, action) {
+      const tokenDecoded = getDecodedAccessToken(action.payload);
+      state.isAuthenticated = true;
+      state.tokenHashed = action.payload;
+      state.role = tokenDecoded.role;
+      state.userId = tokenDecoded.userId;
+      state.avatarPath = tokenDecoded.avatarPath;
+      state.email = tokenDecoded.email;
+      state.firstname = tokenDecoded.firstname;
+      state.lastname = tokenDecoded.lastname;
+      state.exp = tokenDecoded.exp;
+      state.iat = tokenDecoded.iat;
+      state.errorFetchingLogin = '';
     },
     logout(state) {
-      state.user = '';
       state.isAuthenticated = false;
+      state.tokenHashed = '';
+      state.role = '';
+      state.userId = null;
+      state.avatarPath = '';
+      state.email = '';
+      state.firstname = '';
+      state.lastname = '';
+      state.exp = null;
+      state.iat = null;
+    },
+    error(state, action) {
+      state.errorFetchingLogin = action.payload;
     },
   },
 });
+type AppDispatch = (args: any) => any;
 
-export const { login, logout } = authenticateSlice.actions;
+export function login(email: string, password: string) {
+  const postData: LoginDto = {
+    email: email,
+    password: password,
+  };
+  return async function (dispatch: AppDispatch, getState: any) {
+    fetch('http://localhost:3000/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(postData),
+    })
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'authenticate/saveToken', payload: data.accessToken }))
+      .catch(() => dispatch({ type: 'authenticate/error', payload: 'Une erreur est apparut lors de la connexion' }));
+  };
+}
+
+export const getDecodedAccessToken = (token: string): any => {
+  try {
+    return jwtDecode(token);
+  } catch (Error) {
+    return null;
+  }
+};
+
+export const { logout } = authenticateSlice.actions;
 export default authenticateSlice.reducer;
