a2214777413ad701d72da8904b1dc7e79b6a8f6f
CanseFr <jucansell@gmail.com>
2025-05-10 11:51:43 +0200

    :sparkles: Data grid for user

--- apps/client/src/pages/admin/users/index.tsx
+++ apps/client/src/pages/admin/users/index.tsx
@@ -1,3 +1,80 @@
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { useEffect, useState } from 'react';
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 90 },
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 110,
+    editable: true,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params: GridValueGetterParams) => `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+  },
+];
+
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
+
 export const ManageUsers = () => {
-  return <p>Managee users</p>;
+  const [users, setUsers] = useState();
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users').then((data) => console.log(data));
+    // fetch('http://localhost:3000/users').then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <>
+      <Box sx={{ marginTop: '5vh', marginBottom: '2vh', height: 400, width: '100%' }}>
+        <Typography textAlign="center" variant="h3">
+          Gestion des Utilisateurs
+        </Typography>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          pageSizeOptions={[5]}
+          checkboxSelection
+          disableRowSelectionOnClick
+        />
+      </Box>
+    </>
+  );
 };
